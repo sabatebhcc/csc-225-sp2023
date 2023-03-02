ef2a2a7b0fae92358fb07d3d20a230c5dbb61d1f
sabatebhcc <125384585+sabatebhcc@users.noreply.github.com>
2023-03-02 08:50:15 -0500

    Create exam1.py

---
+++ exams/exam1.py
@@ -0,0 +1,22 @@
+# 1. Consider the following sequence: 1, 4, 9, 16, 25, 36, 49.... The sequence
+#    is simply the square of the natural numbers. Write a function that takes a
+#    number as input and returns the sum of the above sequence up to that
+#    index. For example, input 1, simply returns 1. input 2, returns 1 + 4 = 5.
+
+
+# 2. Write a Python function that takes two lists of integers as input and
+#    returns a list containing the intersection of the two lists.
+
+# 3. Write a Python program that takes a string as input and counts the number
+#    of vowels in the string.
+
+# 4. The numerical value of a string is defined as the sum of all the numerical
+#    values of its letters where a = 1, b =2, ..., z = 26. Write a Python
+#    program that takes a string as input and returns the numerical value of the
+#    string.
+
+# 5. Write a class called Queue which implements a queue data structure with the
+#    following methods:
+#    Add(x) -> Insert the number x to the back of the queue.
+#    Pop() --> Removes and returns the element at the front of the queue.
+#    Size() --> Returns the current number of elements in the queue.
